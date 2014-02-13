94566ffcaa819a89851463dfc8baad3a2b17464b
Julio Brana <juliobranha@gmail.com>
2014-02-13 20:49:55 +0000

    Client custom field created. Scripts added.

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'rrdcreative');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'jb');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'cometaG');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
