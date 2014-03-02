26f6572713140088801204ae981252bb5c84fef9
Alvaro B <cobianzo@gmail.com>
2014-03-02 14:47:36 +0000

    poner título si es frontpage.

--- wp-content/themes/rrdcreative/header.php
+++ wp-content/themes/rrdcreative/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="X-UA-Compatible" content="" /> 
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    	<title><?php echo wp_title('');  ?></title>
+    	<title><?php echo ($a = wp_title(''))? $a : get_bloginfo('blogname');  ?></title>
     
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=2.0, user-scalable=0">
     <?php wp_head(); ?> <?php /* this is used by many Wordpress features and for plugins to work proporly */ ?>
